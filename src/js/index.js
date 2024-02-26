7bd65090476816a5e3b1338060b9e199f8c05ebd
jiohjung98 <ckaquz98@naver.com>
2024-02-26 20:56:32 +0900

    chore: webpack 이용해 초기 세팅 설정

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import "../css/style.css";
+
+// console.log('hi');
+
+let a = 1;
+let b = 2;
+
+console.log(a+b);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist"),
+        clean: true
+    },
+    // 빌드 파일과 원본 파일 연결
+    devtool: "source-map",
+    // production, development 두 개가 있음
+    mode: "development",
+    plugins: [
+        new HtmlWebpackPlugin({
+             title: "keyboard",
+             template: "./index.html",
+             inject: "body",
+             favicon: "./favicon.ico"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+
+    },
+    optimization: {
+        minimizer: [
+            new TerserWebpackPlugin(),
+            new CssMinimizerWebpackPlugin()
+        ]
+    }
+ } 
