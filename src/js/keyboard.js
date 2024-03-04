eed73cf2bc3314ce3af2dbe8b866d59fb2c6b920
jiohjung98 <ckaquz98@naver.com>
2024-03-04 17:10:54 +0900

    feat: keyboard event 적용

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -2,6 +2,10 @@ export class Keyboard {
     // # - hash: private 변수가 돼서 클래스 외부에서 읽히고 수정될 수 없음
     #swichEl;
     #fontSelectEl;
+    #containerEl;
+    #keyboardEl;
+    #inputGroupEl;
+    #inputEl;
 
     constructor() {
         this.#assignElement();
@@ -9,22 +13,54 @@ export class Keyboard {
     }
 
     #assignElement() {
-        this.#swichEl = document.getElementById('switch');
-        this.#fontSelectEl = document.getElementById('font');
+        this.#containerEl = document.getElementById('container');
+        this.#swichEl = this.#containerEl.querySelector('#switch');
+        this.#fontSelectEl = this.#containerEl.querySelector('#font');
+        this.#keyboardEl = this.#containerEl.querySelector('#keyboard');
+        this.#inputGroupEl = this.#containerEl.querySelector('#input-group');
+        this.#inputEl = this.#inputGroupEl.querySelector('#input');
     }
 
     #addEvent() {
-        this.#swichEl.addEventListener("change", (event) => {
-            document.documentElement.setAttribute(
-                "theme",
-                event.target.checked ? "dark-mode": ""
-            )
-            console.log(event.target.checked);
-        });
+        this.#swichEl.addEventListener("change", this.#onChangeTheme);
+        this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+        document.addEventListener('keydown', this.#onKeyDown.bind(this));
+        document.addEventListener('keyup', this.#onKeyUp.bind(this));
+        this.#inputEl.addEventListener('input', this.#onInput);
+    };
 
-        this.#fontSelectEl.addEventListener("change", (event) => {
-            document.body.style.fontFamily = event.target.value;
-            console.log(event.target.value);
-        })
+    #onInput(event) {
+            this.#inputEl.value = this.#inputEl.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+    };
+
+    #onKeyUp(event) {
+            console.log('keyup');
+            console.log(event.code);
+            this.#keyboardEl
+                .querySelector(`[data-code=${event.code}]`)
+                ?.classList.remove('active');
+    };
+
+    #onKeyDown(event) {
+            console.log('keydown');
+            console.log(event.code);
+            
+            this.#inputGroupEl.classList.toggle('error', /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key));
+
+            this.#keyboardEl
+                .querySelector(`[data-code=${event.code}]`)
+                ?.classList.add('active');
+    };
+
+    #onChangeTheme(event) {
+        document.documentElement.setAttribute(
+            "theme",
+            event.target.checked ? "dark-mode": ""
+        )
+    }
+
+    #onChangeFont(event) {
+        document.body.style.fontFamily = event.target.value;
+        console.log(event.target.value);
     }
 }
